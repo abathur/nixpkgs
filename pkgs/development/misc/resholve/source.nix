6fd9f94fa8e45b86eb855d54c6773244ad526c1c
Travis A. Everett <travis.a.everett@gmail.com>
2021-08-01 20:46:47 -0500

    resholve: 0.5.1 -> 0.6.0 +refactor +binlore
    A bit going on here.
    - Updating resholve.
      - adding a package, `binlore`, which provides ~intel on executables
        to support new functionality in resholve
      - adding a package, `yallback`, which provides rule-based callbacks
        for YARA rule matches (for `binlore`).
      - automatically generating "lore" for each `input` to a solution in
        `resholvePackage`.
      - update README
    - restructuring some of the nix components to better support
      my local dev and CI workflows.
      - moved package tests into passthru/tests.nix (cuts `bats` out of
        resholve's immediate dependencies, makes it possible to add my
        existing Nix API test).
      - move my oil-dev patches out of resholve into a separate repo (so
        oil doesn't have to rebuild every time resholve's source changes).
        Also moving oil-dev into its own Nix file here, to ~track the
        default.nix in its own repo.

---
+++ pkgs/development/misc/resholve/source.nix
@@ -0,0 +1,19 @@
+{ fetchFromGitHub
+, ...
+}:
+
+rec {
+  version = "0.6.0-rc.1";
+  rSrc =
+    # local build -> `make ci`; `make clean` to restore
+    # return to remote source
+    # if builtins.pathExists ./.local
+    # then ./.
+    # else
+      fetchFromGitHub {
+        owner = "abathur";
+        repo = "resholve";
+        rev = "v${version}";
+        hash = "sha256-3/aGtGP3YgxZbdyUYS7jBuVixalBBQMFCX5a8SfhhYM=";
+      };
+}
