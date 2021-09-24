d9dcbb0fb202e8253759434910904da8431a784e
Travis A. Everett <travis.a.everett@gmail.com>
2021-09-23 21:28:02 -0500

    WIP

--- pkgs/tools/admin/acme.sh/default.nix
+++ pkgs/tools/admin/acme.sh/default.nix
@@ -1,39 +1,67 @@
-{ stdenv, lib, fetchFromGitHub, makeWrapper, curl, openssl, socat, iproute2,
-  unixtools, dnsutils, coreutils, gnugrep, gnused }:
-stdenv.mkDerivation rec {
+{ stdenv
+, lib
+, resholvePackage
+, fetchFromGitHub
+, bash
+, curl
+, openssl_1_1
+, socat
+, iproute2
+, unixtools
+, dnsutils
+, coreutils
+, gnugrep
+, gnused
+, apacheHttpd
+}:
+resholvePackage rec {
   pname = "acme.sh";
-  version = "3.0.0";
+  version = "2.9.0";
 
   src = fetchFromGitHub {
     owner = "Neilpang";
     repo = "acme.sh";
     rev = version;
-    sha256 = "sha256-KWSDAHzvNl8Iao13OV/ExRoKqkc9nouWim+bAN1V+Jo=";
+    sha256 = "sha256-BSKqfj8idpE4OV8/EJkCFo5i1vq/aEde/moqJcwuDvk=";
   };
-
-  nativeBuildInputs = [ makeWrapper ];
 
   installPhase = ''
     runHook preInstall
 
     mkdir -p $out $out/bin $out/libexec
     cp -R $src/* $_
-    makeWrapper $out/libexec/acme.sh $out/bin/acme.sh \
-      --prefix PATH : "${
-        lib.makeBinPath [
-          coreutils
-          gnugrep
-          gnused
-          socat
-          openssl
-          curl
-          dnsutils
-          (if stdenv.isLinux then iproute2 else unixtools.netstat)
-        ]
-      }"
+    cp $out/libexec/acme.sh $out/bin/acme.sh
 
     runHook postInstall
   '';
+  solutions = {
+    acme = {
+      interpreter = "${bash}/bin/sh";
+      scripts = [ "bin/acme.sh" ];
+      inputs = [
+        coreutils
+        gnugrep
+        gnused
+        socat
+        openssl_1_1
+        curl
+        dnsutils
+        (if stdenv.isLinux then iproute2 else unixtools.netstat)
+        apacheHttpd
+      ];
+      fix = {
+        "$ACME_OPENSSL_BIN" = [ "openssl" ];
+        "$_APACHECTL" = [ "apachectl" ];
+      };
+      keep = {
+        # TODO: unsure about this; it's trying to detect
+        # the use of this flag; I think at least darwin
+        # will have to fall back on system logger.
+        # darwin does support -i, but IDK who doesn't...
+        "$__logger_i" = true;
+      };
+    };
+  };
 
   meta = with lib; {
     description = "A pure Unix shell script implementing ACME client protocol";
