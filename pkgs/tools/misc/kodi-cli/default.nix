71bd893b6c19889bb90bb803005f03cf50cca6e0
Travis A. Everett <travis.a.everett@gmail.com>
2022-01-29 10:35:06 -0600

    kodi-cli: use resholve.mkDerivation

--- pkgs/tools/misc/kodi-cli/default.nix
+++ pkgs/tools/misc/kodi-cli/default.nix
@@ -1,6 +1,18 @@
-{ lib, stdenv, fetchFromGitHub, makeWrapper, curl, bash, jq, youtube-dl, gnome }:
+{ lib
+, resholve
+, fetchFromGitHub
+, bash
+, curl
+, gawk
+, coreutils
+, mps-youtube
+, gnugrep
+, gnome
+, youtube-dl
+, jq
+}:
 
-stdenv.mkDerivation rec {
+resholve.mkDerivation rec {
   pname = "kodi-cli";
   version = "1.1.1";
 
@@ -11,15 +23,44 @@ stdenv.mkDerivation rec {
     sha256 = "0f9wdq2fg8hlpk3qbjfkb3imprxkvdrhxfkcvr3dwfma0j2yfwam";
   };
 
-  nativeBuildInputs = [ makeWrapper ];
-
   installPhase = ''
     mkdir -p $out/bin
     cp -a kodi-cli $out/bin
-    wrapProgram $out/bin/kodi-cli --prefix PATH : ${lib.makeBinPath [ curl bash ]}
     cp -a playlist_to_kodi $out/bin
-    wrapProgram $out/bin/playlist_to_kodi --prefix PATH : ${lib.makeBinPath [ curl bash gnome.zenity jq youtube-dl ]}
   '';
+
+  solutions = {
+    default = {
+      scripts = [ "bin/kodi-cli" "bin/playlist_to_kodi" ];
+      interpreter = "${bash}/bin/bash";
+      inputs = [
+        curl
+        gawk
+        coreutils
+        mps-youtube
+        gnugrep
+        gnome.zenity
+        youtube-dl # staying faithful to existing package, but it seems like everyone's moved on
+        jq
+        "bin" # our own internal executables
+      ];
+      execer = [
+        /*
+        youtube-dl has args that may exec, but playlist_to_kodi
+        doesn't use them. Need to override lore (until/unless
+        resholve handles it: TODO issue link)
+        */
+        "cannot:${youtube-dl}/bin/youtube-dl"
+        /*
+        mpsyt has at least two subcommands for supplying
+        a downloader and player executable, but I don't see either
+        in use here.
+        */
+        "cannot:${mps-youtube}/bin/mpsyt"
+        "cannot:bin/kodi-cli" # don't see any obvious root arg exec
+      ];
+    };
+  };
 
   meta = with lib; {
     homepage = "https://github.com/nawar/kodi-cli";
