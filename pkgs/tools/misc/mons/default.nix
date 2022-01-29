d712837f61103cbeaf63d6c2fcf1927527e7a69e
Travis A. Everett <travis.a.everett@gmail.com>
2022-01-29 11:26:45 -0600

    mons: use resholve.mkDerivation

--- pkgs/tools/misc/mons/default.nix
+++ pkgs/tools/misc/mons/default.nix
@@ -1,6 +1,17 @@
-{ stdenv, lib, fetchFromGitHub, help2man, xrandr }:
+{ resholve
+, lib
+, fetchFromGitHub
+, fetchpatch
+, help2man
+, bash
+, coreutils
+, gnused
+, xrandr
+, gnugrep
+, gawk
+}:
 
-stdenv.mkDerivation rec {
+resholve.mkDerivation rec {
   pname = "mons";
   version = "20200320";
 
@@ -12,14 +23,17 @@ stdenv.mkDerivation rec {
     fetchSubmodules = true;
   };
 
-  patches = [
-    # Substitute xrandr path with @xrandr@ so we can replace it with
-    # real path in substituteInPlace
-    ./xrandr.patch
-  ];
-
+  /*
+    Patching out this check; it's bad on multiple levels:
+    1. The check literally breaks if it fails.
+       See https://github.com/Ventto/mons/pull/49
+    2. It doesn't need to do this; source would fail with a
+       sensible message if the script was missing.
+    3. resholve can't wrestle with test/[] (at least until
+       https://github.com/abathur/resholve/issues/78)
+  */
   postPatch = ''
-    substituteInPlace mons.sh --replace '@xrandr@' '${xrandr}/bin/xrandr'
+    substituteInPlace mons.sh --replace '[ ! -r "$lib" ] && { "$lib: library not found."; exit 1; }' ""
   '';
 
   nativeBuildInputs = [ help2man ];
@@ -27,6 +41,44 @@ stdenv.mkDerivation rec {
     "DESTDIR=$(out)"
     "PREFIX="
   ];
+
+  solutions = {
+    mons = {
+      scripts = [ "bin/mons" "lib/libshlist/liblist.sh" ];
+      interpreter = "${bash}/bin/sh";
+      inputs = [
+        coreutils
+        gnused
+        xrandr
+        gnugrep
+        gawk
+        bash
+      ];
+      fix = {
+        "$lib" = [ "lib/libshlist/liblist.sh" ];
+        "$XRANDR" = [ "xrandr" ];
+      };
+      keep = {
+        /*
+        has a whole slate of *flag variables that it sets to either
+        the true or false builtin and then executes...
+        */
+        "$aFlag" = true;
+        "$dFlag" = true;
+        "$eFlag" = true;
+        "$mFlag" = true;
+        "$nFlag" = true;
+        "$oFlag" = true;
+        "$sFlag" = true;
+        "$OFlag" = true;
+        "$SFlag" = true;
+        "$pFlag" = true;
+        "$iFlag" = true;
+        "$xFlag" = true;
+        "$is_flag" = true;
+      };
+    };
+  };
 
   meta = with lib; {
     description = "POSIX Shell script to quickly manage 2-monitors display";
