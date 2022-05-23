f15182360eeb8f83c736ffab26b7afaf35862892
PedroHLC ☭ <root@pedrohlc.com>
2022-05-23 17:34:12 -0300

    airgeddon: init at 11.01

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1063,6 +1063,8 @@ with pkgs;
 
   airfield = callPackage ../tools/networking/airfield { };
 
+  airgeddon = callPackage ../tools/networking/airgeddon { };
+
   apache-airflow = with python3.pkgs; toPythonApplication apache-airflow;
 
   airsonic = callPackage ../servers/misc/airsonic { };
