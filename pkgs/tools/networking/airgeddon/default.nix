0f781bc4a9ccdc6e8a3150304d084e199d210be9
Travis A. Everett <travis.a.everett@gmail.com>
2022-05-24 23:40:45 -0500

    WIP

--- pkgs/tools/networking/airgeddon/default.nix
+++ pkgs/tools/networking/airgeddon/default.nix
@@ -205,8 +205,8 @@ resholve.mkDerivation rec {
         "$AIRGEDDON_MDK_VERSION" = [ "mdk4" ];
         "$AIRGEDDON_PLUGINS_ENABLED" = [ "true" ];
 
-        "$scriptfolder" = [ "${placeholder "out"}/lib/airgeddon/" ];
-        "$language_strings_file" = [ "language_strings.sh" ];
+        "$scriptfolder" = [ "" ];
+        #"$language_strings_file" = [ "language_strings.sh" ];
         "$known_pins_dbfile" = [ "known_pins.db" ];
 
         # Thank god there is only one plugin
